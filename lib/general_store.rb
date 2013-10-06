2fec9e7cf2f7e75530d739460aa6b47e746a81f0
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-06 13:15:35 -0400

    reorganize general_store file

--- lib/general_store.rb
+++ lib/general_store.rb
@@ -4,18 +4,6 @@ require "ostruct"
 class GeneralStore
   attr_accessor :config
 
-  def initialize config_contents
-    self.config = config_contents
-    config.each do |k,v|
-      accessor = k.to_sym
-      self.class.class_eval do
-        attr_accessor accessor
-      end
-      send("#{accessor}=", v)
-    end
-  end
-  private_class_method :new
-
   def self.read dir_name
     new YAML.load_file config_file dir_name
   rescue Errno::ENOENT
@@ -30,6 +18,24 @@ class GeneralStore
     new(ostruct.to_h).set dir
   end
 
+  def set dir
+    klass = self.class
+    klass.write_file klass.config_file(dir), config
+  end
+  private :set
+
+  def initialize config_contents
+    self.config = config_contents
+    config.each do |k,v|
+      accessor = k.to_sym
+      self.class.class_eval do
+        attr_accessor accessor
+      end
+      send("#{accessor}=", v)
+    end
+  end
+  private_class_method :new
+
   def self.config_file dir_name
     File.expand_path File.join dir_name, "config.yml"
   end
@@ -39,12 +45,6 @@ class GeneralStore
     File.expand_path @dir
   end
   private_class_method :config_dir
-
-  def set dir
-    klass = self.class
-    klass.write_file klass.config_file(dir), config
-  end
-  private :set
 
   def self.create_config_file
     check_dir_existence
