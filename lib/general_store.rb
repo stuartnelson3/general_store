2b8fa35afb38eb998784da5454e2fd6c15052a3c
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-06 13:14:27 -0400

    only exposing .create and .read class methods

--- lib/general_store.rb
+++ lib/general_store.rb
@@ -14,19 +14,12 @@ class GeneralStore
       send("#{accessor}=", v)
     end
   end
+  private_class_method :new
 
   def self.read dir_name
     new YAML.load_file config_file dir_name
   rescue Errno::ENOENT
     puts 'You need to setup your General Store first!'
-  end
-
-  def self.config_file dir_name
-    File.expand_path File.join dir_name, "config.yml"
-  end
-
-  def self.config_dir
-    File.expand_path @dir
   end
 
   def self.create dir
@@ -37,27 +30,41 @@ class GeneralStore
     new(ostruct.to_h).set dir
   end
 
+  def self.config_file dir_name
+    File.expand_path File.join dir_name, "config.yml"
+  end
+  private_class_method :config_file
+
+  def self.config_dir
+    File.expand_path @dir
+  end
+  private_class_method :config_dir
+
   def set dir
     klass = self.class
     klass.write_file klass.config_file(dir), config
   end
+  private :set
 
   def self.create_config_file
     check_dir_existence
     check_file_existence
   end
+  private_class_method :create_config_file
 
   def self.write_file file, data
     File.open file, File::RDWR|File::TRUNC|File::CREAT, 0600 do |config|
       config.write YAML.dump data
     end
   end
+  private_class_method :write_file
 
   def self.check_dir_existence
     unless Dir.exists? config_dir
       Dir.mkdir config_dir
     end
   end
+  private_class_method :check_dir_existence
 
   def self.check_file_existence
     file = config_file @dir
@@ -65,4 +72,5 @@ class GeneralStore
       write_file file, {}
     end
   end
+  private_class_method :check_file_existence
 end
